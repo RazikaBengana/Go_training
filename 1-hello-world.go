8e6f5f0667ef248b0c8f7537fb6e72a3e4b27f64
RazikaBengana <razikabengana@gmail.com>
2024-02-16 22:02:28 +0100

    `Hello, world!` in Go

---
+++ 1-hello-world.go
@@ -0,0 +1,14 @@
+// Déclaration du package principal
+// C'est le point d'entrée pour l'exécution du programme
+package main
+
+// Importation du package "fmt"
+// Il est utilisé pour les opérations d'entrée/sortie formatées, comme l'impression de messages dans la console
+import "fmt"
+
+// La fonction "main" est le point d'entrée du programme
+func main() {
+
+	// fonction "Println" (du package "fmt") --> fonction d'impression de message, suivi d'un retour à la ligne
+	fmt.Println("Hello, world!")
+}
